0108ed01e4060c94ab02480576b48a7659042b57
Adolfo Fitoria <adolfo.fitoria@gmail.com>
2010-11-03 11:37:57 -0600

    Cambiando el locale a Español

--- settings.py
+++ settings.py
@@ -15,7 +15,7 @@ TIME_ZONE = 'America/Managua'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-ni'
 
 SITE_ID = 1
 
