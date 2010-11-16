300c3074c1b797863e9fb32fe5ea448411e2caa7
Helmy Giacoman <helmygb@gmail.com>
2010-11-16 16:10:56 -0600

    sirviendo archivos estaticos

--- urls.py
+++ urls.py
@@ -1,25 +1,32 @@
 from django.conf.urls.defaults import *
-
-# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
+import os
+PROJECT_DIR = os.path.dirname(__file__)
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Example:
-    (r'^mapeo/', include('maonic.mapeo.urls')),
+                       # Example:
+                       (r'^mapeo/', include('maonic.mapeo.urls')),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+                       # Uncomment the admin/doc line below to enable admin documentation:
+                       (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    # Uncomment the next line to enable the admin:
-    (r'^admin/', include(admin.site.urls)),
-    (r'^accounts/', include('registration.urls')),
-)
+                       # Uncomment the next line to enable the admin:
+                       (r'^admin/', include(admin.site.urls)),
+                       (r'^accounts/', include('registration.urls')),
+                       )
 
 urlpatterns += patterns('',
-  (r'^accounts/profile/$', direct_to_template, {'template': 'registration/profile.html'}),
-  (r'^accounts/password_reset/$', password_reset, {'template_name': 'registration/password_reset.html'}),
-  (r'^accounts/password_reset_done/$', password_reset_done, {'template_name': 'registration/password_reset_done.html'}),
-  (r'^accounts/password_change/$', password_change, {'template_name': 'registration/password_change.html'}),
-  (r'^accounts/password_change_done/$', password_change_done, {'template_name': 'registration/password_change_done.html'}),
-)
+                        (r'^accounts/profile/$', direct_to_template, {'template': 'registration/profile.html'}),
+                        (r'^accounts/password_reset/$', password_reset, {'template_name': 'registration/password_reset.html'}),
+                        (r'^accounts/password_reset_done/$', password_reset_done, {'template_name': 'registration/password_reset_done.html'}),
+                        (r'^accounts/password_change/$', password_change, {'template_name': 'registration/password_change.html'}),
+                        (r'^accounts/password_change_done/$', password_change_done, {'template_name': 'registration/password_change_done.html'}),
+                        )
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+                            (r'^files/(.*)$', 'django.views.static.serve',
+                            {'document_root': PROJECT_DIR + '/media'}),
+                            )
