c6634c95eac37489286666fe3dbcb921c2555cac
Helmy Giacoman <helmygb@gmail.com>
2010-11-16 17:16:12 -0600

    creando primeras plantillas

---
+++ views.py
@@ -0,0 +1,7 @@
+# -*- coding: UTF-8 -*-
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+
+def index(request):    
+    return render_to_response('index.html', RequestContext(request, locals()))
+
