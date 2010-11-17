e5d256721c5a2c2000f5a59f2ff9ffaff42ccb4c
Adolfo Fitoria <adolfo.fitoria@gmail.com>
2010-11-17 10:36:59 -0600

    agregando cosas al admin

--- mapeo/admin.py
+++ mapeo/admin.py
@@ -1,5 +1,4 @@
 # -*- coding: UTF-8 -*-
-
 from django.contrib import admin
 from models import *
 
@@ -12,5 +11,12 @@ admin.site.register(ComProducto)
 admin.site.register(Certificadora)
 admin.site.register(Financiera)
 admin.site.register(OrgPublica)
-
-
+admin.site.register(RubroCultivo)
+admin.site.register(RubroArboles)
+admin.site.register(RubroAnimales)
+admin.site.register(Semilla)
+admin.site.register(MateriaProcesada)
+admin.site.register(BuenasPracticas)
+admin.site.register(TipoOrganizacion)
+admin.site.register(AreaTrabajo)
+admin.site.register(TipoUsuario)
